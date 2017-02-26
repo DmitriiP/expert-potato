8ef59b7afbd37c3970636a13367eb40ea2b8c5e3
Dmitrii Prihodco <dmitrii.prihodco@gmail.com>
2017-02-26 23:42:11 +0200

    Basic working setup of webpack.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # expert-potato
+Made this project to update myself a bit with more modern web stuff.
+So far I'll be trying TypeScript + Webpack + React/Redux
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+
+module.exports = {
+    entry: "./front/entry.ts",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {test: /\.tsx?$/, use: 'ts-loader'}
+        ]
+    }
+};
